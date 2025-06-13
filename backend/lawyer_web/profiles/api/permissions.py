1a1487a66f1fdd4851b15af38566b378fe8cab52
Punisher <alex_77_90@mail.ru>
2025-06-13 19:41:53 +0500

    fix permission

--- backend/lawyer_web/profiles/api/permissions.py
+++ backend/lawyer_web/profiles/api/permissions.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class HasHeaderReact(BasePermission):
 
-    def has_header(self, request, view):
+    def has_permission(self, request, view):
         react_header = request.headers.get('X-Superuser-Access')
+        print(f'{react_header = }')
         return react_header == 'hjflSdhjlkSDfjo79sdffs009fs87s0df09s8d'
--- backend/lawyer_web/profiles/api/views.py
+++ backend/lawyer_web/profiles/api/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from .permissions import HasHeaderReact
 from .serializers import UserSerializer
@@ -8,5 +9,5 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.select_related('profile').all()
     serializer_class = UserSerializer
     http_method_names = ['get',]
-    permission_classes = [HasHeaderReact]
+    permission_classes = [HasHeaderReact | IsAdminUser]
 
